0623fe20fb86ed76db2a1badf51af8558e6e0962
Ravi shah <44101788+iamshahh@users.noreply.github.com>
2020-03-04 12:10:39 -0800

    Updated new Subnet

--- asg.tf
+++ asg.tf
@@ -7,6 +7,6 @@ resource "aws_autoscaling_group" "default" {
   desired_capacity          = 1
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
-  vpc_zone_identifier       = ["subnet-9db355bc","subnet-03305080190a6a38f"]
+  vpc_zone_identifier       = ["subnet-080e2e52702b495dd","subnet-023310aea4f0d8bc4"]
 #  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
 }
