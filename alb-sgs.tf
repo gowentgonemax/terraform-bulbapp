ae3b75a38a31bca3fb10ed8185bf96eb0f04e5f5
Ravi shah <44101788+iamshahh@users.noreply.github.com>
2020-03-04 12:07:22 -0800

    Update alb-sgs.tf

--- alb-sgs.tf
+++ alb-sgs.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "default_lb" {
   name        = "terraform-ecs-bulbapp-lb"
   description = "Allow traffic"
-  vpc_id      = "vpc-8c1e2af6"
+  vpc_id      = "vpc-081b47aa0b157d067"
 
   ingress {
     from_port   = 80
